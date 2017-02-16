996f367691ae1b3aeeb8f82a3141524e3b771da5
dangerstephen <dangersteve1@gmail.com>
2017-02-16 12:34:40 -0800

    added some chat css and javascript for logic of chat

--- js/app.js
+++ js/app.js
@@ -1,154 +1,89 @@
 $(document).ready(function() {
-    "use strict";
 
-
-    $('.background-image-holder').each(function() {
-        var imgSrc = $(this).children('img').attr('src');
-        $(this).css('background', 'url("' + imgSrc + '")');
-        $(this).children('img').hide();
-        $(this).css('background-position', 'initial');
+    $('.wont-chat').on('click', function() {
+        $('section').removeClass('hidden');
+        //  $('.wont-chat').addClass('hidden');
     });
 
-    // Fade in background images
+    $('.next-chat-group').on('click', function() {
+        console.log("i was clicked");
+        $('#chat-window').find('#user-name').append('<div class="chat-new">ah come on now, its just a name.</div>');
+        console.log("i was added");
+    });
 
-    setTimeout(function() {
-        $('.background-image-holder').each(function() {
-            $(this).addClass('fadeIn');
+    // Data bindings
+    $('[sendData]').each(function() {
+        var provider = "facebook";
+        var watch = $($(this).attr('sendData'));
+        var watcher = $(this);
+        watch.on('keyup', function() {
+            watcher.text($(this).val())
+        })
+    });
+
+    // Chat-style form stuff
+    if ($('.chat-window').length) {
+        $('.chat-group').not('[data-active]').hide();
+        $('#chat-window .error').hide();
+
+        $('#chat-window').on('submit', function(e) {
+            e.preventDefault();
         });
-    }, 200);
 
-    // Multipurpose Modals
+        function checkErrors() {
+            var name = $('#chat-window').find('#user-name').val();
+            var vistingReason = $('#chat-window').find('#vistingReason').val();
 
-    jQuery('.foundry_modal[modal-link]').remove();
+            var errors = [];
+            $('.with-error').removeClass('with-error');
 
-    if($('.foundry_modal').length && (!jQuery('.modal-screen').length)){
-        // Add a div.modal-screen if there isn't already one there.
-        var modalScreen = jQuery('<div />').addClass('modal-screen').appendTo('body');
+            if (!name && $('#chat-window').find('#user-name').is(':visible')) {
+                errors.name = "ah come on now, its just a name.";
+                $('#chat-window').find('#user-name').parent().parent().parent().addClass('with-error').append('<div class="inline-error">ah come on now, its just a name.</div>');
+            } else {
+                $('#chat-window').find('#user-name').parent().parent().parent().find('.inline-error').remove();
+            }
 
+            if (!vistingReason && $('#chat-window').find('#vistingReason').is(':visible')) {
+                errors.vistingReason = "is fairly simple isnt it? either yes or no, will do";
+                $('#chat-window').find('#vistingReason').parent().parent().parent().addClass('with-error').append('<div class="inline-error">either yes or no, will do. its fairly simple isnt it?</div>');
+            } else {
+                $('#chat-window').find('#vistingReason').parent().parent().parent().find('.inline-error').remove();
+            }
+
+            return errors;
+        }
+
+        $('[data-show="next-chat-group"]').on('click', function(e) {
+            e.preventDefault();
+            var errors = [];
+            errors = checkErrors();
+            if ($.isEmptyObject(errors)) {
+                $('#chat-window').find('#user-name').parent().parent().parent().append(
+                '<div class="chat-group"><div class="message messageClient"><div class="bubble bubbleClient"><p class="chat-text">Nice to meet you <span sendData="#user-name"></span>! Im guessing you came to my site to learn about me. Is that right?</p></div></div>',
+                '<div class="message messageUser"><div class="bubble bubbleUser"><p class="chat-text"><div class="chat-input chat-input--required"><input type="text" onclick="reasonBlinker()" class="chat-input--text" id="vistingReason" name="vistingReason" placeholder="yes or no" data-required /></div></p><img type="submit" data-show="next-chat-group"src="img/up.png"></img></div></div></div>'
+            );
+            }
+        });
     }
-
-    jQuery('.foundry_modal').click(function(){
-        jQuery(this).addClass('modal-acknowledged');
-    });
-
-    jQuery(document).on('wheel mousewheel scroll', '.foundry_modal, .modal-screen', function(evt){
-        $(this).get(0).scrollTop += (evt.originalEvent.deltaY);
-        return false;
-    });
-
-    $('.modal-container:not([modal-link])').each(function(index) {
-        if(jQuery(this).find('iframe[src]').length){
-        	jQuery(this).find('.foundry_modal').addClass('iframe-modal');
-        	var iframe = jQuery(this).find('iframe');
-        	iframe.attr('data-src',iframe.attr('src'));
-            iframe.attr('src', '');
-
-        }
-        jQuery(this).find('.btn-modal').attr('modal-link', index);
-
-        // Only clone and append to body if there isn't already one there
-        if(!jQuery('.foundry_modal[modal-link="'+index+'"]').length){
-            jQuery(this).find('.foundry_modal').clone().appendTo('body').attr('modal-link', index).prepend(jQuery('<i class="ti-close close-modal">'));
-        }
-    });
-
-    $('.btn-modal').unbind('click').click(function(){
-    	var linkedModal = jQuery('.foundry_modal[modal-link="' + jQuery(this).attr('modal-link') + '"]'),
-            autoplayMsg = "";
-        jQuery('.modal-screen').toggleClass('reveal-modal');
-        if(linkedModal.find('iframe').length){
-            if(linkedModal.find('iframe').attr('data-autoplay') === '1'){
-                var autoplayMsg = '&autoplay=1'
-            }
-        	linkedModal.find('iframe').attr('src', (linkedModal.find('iframe').attr('data-src') + autoplayMsg));
-        }
-        if(linkedModal.find('video').length){
-            linkedModal.find('video').get(0).play();
-        }
-        linkedModal.toggleClass('reveal-modal');
-        return false;
-    });
-
-
-
-    // Exit modals
-    $('.foundry_modal[data-show-on-exit]').each(function(){
-        var modal = $(this);
-        var exitSelector = $(modal.attr('data-show-on-exit'));
-        // If a valid selector is found, attach leave event to show modal.
-        if($(exitSelector).length){
-            modal.prepend($('<i class="ti-close close-modal">'));
-            $(document).on('mouseleave', exitSelector, function(){
-                if(!$('body .reveal-modal').length){
-                    if(typeof modal.attr('data-cookie') !== typeof undefined){
-                        if(!mr_cookies.hasItem(modal.attr('data-cookie'))){
-                            modal.addClass('reveal-modal');
-                            $('.modal-screen').addClass('reveal-modal');
-                        }
-                    }else{
-                        modal.addClass('reveal-modal');
-                        $('.modal-screen').addClass('reveal-modal');
-                    }
-                }
-            });
-        }
-    });
-
-    // Autoclose modals
-
-    $('.foundry_modal[data-hide-after]').each(function(){
-        var modal = $(this);
-        var delay = modal.attr('data-hide-after');
-        if(typeof modal.attr('data-cookie') != "undefined"){
-            if(!mr_cookies.hasItem(modal.attr('data-cookie'))){
-                setTimeout(function(){
-                if(!modal.hasClass('modal-acknowledged')){
-                    modal.removeClass('reveal-modal');
-                    $('.modal-screen').removeClass('reveal-modal');
-                }
-                },delay);
-            }
-        }else{
-            setTimeout(function(){
-                if(!modal.hasClass('modal-acknowledged')){
-                    modal.removeClass('reveal-modal');
-                    $('.modal-screen').removeClass('reveal-modal');
-                }
-            },delay);
-        }
-    });
-
-    jQuery('.close-modal:not(.modal-strip .close-modal)').unbind('click').click(function(){
-    	var modal = jQuery(this).closest('.foundry_modal');
-        modal.toggleClass('reveal-modal');
-        if(typeof modal.attr('data-cookie') !== "undefined"){
-            mr_cookies.setItem(modal.attr('data-cookie'), "true", Infinity);
-        }
-    	if(modal.find('iframe').length){
-            modal.find('iframe').attr('src', '');
-        }
-        jQuery('.modal-screen').removeClass('reveal-modal');
-    });
-
-    jQuery('.modal-screen').unbind('click').click(function(){
-        if(jQuery('.foundry_modal.reveal-modal').find('iframe').length){
-            jQuery('.foundry_modal.reveal-modal').find('iframe').attr('src', '');
-        }
-    	jQuery('.foundry_modal.reveal-modal').toggleClass('reveal-modal');
-    	jQuery(this).toggleClass('reveal-modal');
-    });
-
-    jQuery(document).keyup(function(e) {
-		 if (e.keyCode == 27) { // escape key maps to keycode `27`
-            if(jQuery('.foundry_modal').find('iframe').length){
-                jQuery('.foundry_modal').find('iframe').attr('src', '');
-            }
-			jQuery('.foundry_modal').removeClass('reveal-modal');
-			jQuery('.modal-screen').removeClass('reveal-modal');
-		}
-	});
-
-
-
-// end of document ready
 });
+
+// function nameBlinker() {
+//     if ($('input[name=user-name]').attr('placeholder')) {
+//         // get the placeholder text
+//         $('input[name=user-name]').attr('placeholder', '');
+//     } else {
+//         $('input[name=user-name]').attr('placeholder', 'Enter your name');
+//     }
+//     setTimeout(nameBlinker, 1200);
+// };
+//
+// function reasonBlinker() {
+//     if ($('input[name=vistingReason]').attr('placeholder')) {
+//         // get the placeholder text
+//         $('input[name=vistingReason]').attr('placeholder', '');
+//     } else {
+//         $('input[name=vistingReason]').attr('placeholder', 'Yes Or No');
+//     }
+//     setTimeout(reasonBlinker, 1200);
+// };
